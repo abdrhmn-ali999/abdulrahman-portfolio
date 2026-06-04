a313afbfc32bee99f1f375eea15f17c571908f6b
abdilrhman <hlt7792@gmail.com>
2026-06-04 14:32:43 +0300

    fix: update lenis package name

--- src/hooks/useLenis.js
+++ src/hooks/useLenis.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
-import Lenis from '@studio-freight/lenis';
-import gsap from 'gsap';
+import Lenis from 'lenis';import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
